fd557ea3ce18b17e681687f64a0c5c49acef6464
Avaer Kazmer <a@modules.io>
2018-03-29 00:22:09 -0400

    Clean up CLI api

--- index.js
+++ index.js
@@ -3,44 +3,49 @@
 const path = require('path');
 const fs = require('fs');
 
-const _cleanType = s => s.replace(/ ML_CALL/, '');
+const dir = process.argv[2];
+if (dir && fs.lstatSync(dir).isDirectory(dir)) {
+  const _cleanType = s => s.replace(/ ML_CALL/, '');
 
-const _parseArgs = args => args.map(arg => {
-  const match = arg.match(/^((?:const )?\S+(?: \*)?)(.+)/);
-  if (!match) {
-    throw new Error('fail: ' + arg);
-  }
-  const type = _cleanType(match[1]);
-  const name = match[2];
-  return {
-    type,
-    name,
-  };
-});
+  const _parseArgs = args => args.map(arg => {
+    const match = arg.match(/^((?:const )?\S+(?: \*)?)(.+)/);
+    if (!match) {
+      throw new Error('fail: ' + arg);
+    }
+    const type = _cleanType(match[1]);
+    const name = match[2];
+    return {
+      type,
+      name,
+    };
+  });
 
-const fns = [];
+  const fns = [];
 
-const files = fs.readdirSync(__dirname);
-for (let i = 0; i < files.length; i++) {
-  const file = files[i];
-  if (/\.h$/.test(file)) {
-    const s = fs.readFileSync(path.join(__dirname, file), 'utf8');
-    const lines = s.split('\n');
-    for (let j = 0; j < lines.length; j++) {
-      const line = lines[j];
-      if (!/\.\.\./.test(line)) {
-        const match = line.match(/ML_API\s+(.+)\s+(.+)\((.+)\)/);
-        if (match) {
-          const argStrings = match[3].split(/,\s+/);
-          fns.push({
-            type: _cleanType(match[1]),
-            name: match[2],
-            args: _parseArgs(argStrings),
-          });
+  const files = fs.readdirSync(dir);
+  for (let i = 0; i < files.length; i++) {
+    const file = files[i];
+    if (/\.h$/.test(file)) {
+      const s = fs.readFileSync(path.join(dir, file), 'utf8');
+      const lines = s.split('\n');
+      for (let j = 0; j < lines.length; j++) {
+        const line = lines[j];
+        if (!/\.\.\./.test(line)) {
+          const match = line.match(/ML_API\s+(.+)\s+(.+)\((.+)\)/);
+          if (match) {
+            const argStrings = match[3].split(/,\s+/);
+            fns.push({
+              type: _cleanType(match[1]),
+              name: match[2],
+              args: _parseArgs(argStrings),
+            });
+          }
         }
       }
     }
   }
-}
 
-console.log('got fns', JSON.stringify(fns, null, 2));
+  console.log(JSON.stringify(fns, null, 2));
+} else {
+  console.warn('usage: index.js <MLSDK headers directory>');
+}
