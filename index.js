f65c4e7317127cc7df302f83df82430f12ebadb1
Avaer Kazmer <a@modules.io>
2018-03-29 02:13:05 -0400

    Checkpoint

--- index.js
+++ index.js
@@ -3,23 +3,55 @@
 const path = require('path');
 const fs = require('fs');
 
+const _cleanType = s => s.replace(/\s+ML_CALL/, '');
+const _cleanName = s => s.replace(/ML_CALL\s+/, '');
+const _getJsType = type => {
+  switch (type) {
+    case 'bool':
+      return 'Boolean';
+    case 'uint32_t':
+    case 'uint64_t':
+    case 'int64_t':
+    case 'uint16_t':
+    case 'MLHandle':
+    case 'MLFileInfo':
+    case 'const MLFileInfo':
+    case 'MLFileDescriptor':
+    case 'MLDispatchErrorCode':
+    case 'MLSurfaceFormat':
+    case 'VkFormat':
+    case 'MLIdentityError':
+    case 'MLInvokeFuture':
+    case 'MLLifecycleErrorCode':
+    case 'MLLifecycleInitArg':
+    case 'const MLLifecycleInitArg':
+    case 'void':
+    case 'MLDataArrayLockResult':
+    case 'MLPlanesQueryResult':
+    case 'MLSharedFileList':
+    case 'MLTokenAgentError':
+      return 'Number';
+    case 'const char':
+      return 'String';
+    default:
+      throw new Error('unknown type: ' + type);
+  }
+};
+const _parseArgs = args => args.map(arg => {
+  const match = arg.match(/^((?:const )?\S+(?: \*)?)(.+)/);
+  if (!match) {
+    throw new Error('fail: ' + arg);
+  }
+  const type = _cleanType(match[1]);
+  const name = match[2];
+  return {
+    type,
+    name,
+  };
+});
+
 const dir = process.argv[2];
 if (dir && fs.lstatSync(dir).isDirectory(dir)) {
-  const _cleanType = s => s.replace(/ ML_CALL/, '');
-
-  const _parseArgs = args => args.map(arg => {
-    const match = arg.match(/^((?:const )?\S+(?: \*)?)(.+)/);
-    if (!match) {
-      throw new Error('fail: ' + arg);
-    }
-    const type = _cleanType(match[1]);
-    const name = match[2];
-    return {
-      type,
-      name,
-    };
-  });
-
   const fns = [];
 
   const files = fs.readdirSync(dir);
@@ -31,13 +63,20 @@ if (dir && fs.lstatSync(dir).isDirectory(dir)) {
       for (let j = 0; j < lines.length; j++) {
         const line = lines[j];
         if (!/\.\.\./.test(line)) {
-          const match = line.match(/ML_API\s+(.+)\s+(.+)\((.+)\)/);
+          const match = line.match(/ML_API\s+((?:const )?\S+(?: \*)?)\s+(.+)\((.+)\)/);
           if (match) {
             const argStrings = match[3].split(/,\s+/);
+            const type = _cleanType(match[1]);
+            const name = _cleanName(match[2]);
+            const args = _parseArgs(argStrings);
+            if (/\(/.test(type) || /ML_CALL/.test(type) || /ML_CALL/.test(name)) {
+              console.warn({line, type, name});
+              throw new Error('failed to gen');
+            }
             fns.push({
-              type: _cleanType(match[1]),
-              name: match[2],
-              args: _parseArgs(argStrings),
+              type,
+              name,
+              args,
             });
           }
         }
@@ -45,7 +84,32 @@ if (dir && fs.lstatSync(dir).isDirectory(dir)) {
     }
   }
 
-  console.log(JSON.stringify(fns, null, 2));
+  let result = '';
+  // console.log('got fns', fns.length);
+  for (let i = 0; i < fns.length; i++) {
+    const fn = fns[i];
+    const {type, name, args} = fn;
+    result += 'NAN_METHOD(' + name + ') {\n';
+    for (let j = 0; j < args.length; j++) {
+      const jsType = _getJsType(type);
+      result += `  const ${type} ${name} = Nan::New<${jsType}>(info[${j}]);\n`;
+    }
+    result += '\n';
+    result += `  ${name}(`;
+    for (let j = 0; j < args.length; j++) {
+      const arg = args[j];
+      const {name} = arg;
+      result += name;
+      if (j !== (args.length - 1)) {
+        result += ', ';
+      }
+    }
+    result += `);\n`;
+    result += '}\n\n';
+  }
+
+  // console.log(JSON.stringify(fns, null, 2));
+  console.log(result);
 } else {
   console.warn('usage: index.js <MLSDK headers directory>');
 }
